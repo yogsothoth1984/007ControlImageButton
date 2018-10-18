0af7109228dc05e8ce909cc543ee90e3430563ea
Max <vloxomire@gmail.com>
2018-10-18 01:12:25 -0300

    0112

--- app/src/main/java/com/example/a007_controlimagebutton/Listener_Llamar.java
+++ app/src/main/java/com/example/a007_controlimagebutton/Listener_Llamar.java
@@ -1,11 +1,15 @@
 package com.example.a007_controlimagebutton;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Listener_Llamar implements View.OnClickListener {
     private MainActivity contextMain;
     private TextView textView;
+    private ImageButton imageButton;
 
     public Listener_Llamar(MainActivity contextMain) {
         this.contextMain = contextMain;
@@ -15,5 +19,9 @@ public class Listener_Llamar implements View.OnClickListener {
     public void onClick(View v) {
         textView = contextMain.findViewById(R.id.textView);
         textView.setText("Llamando");
+
+        imageButton = contextMain.findViewById(R.id.imageButton);
+        //cambio el icono por una foto
+        imageButton.setImageResource(R.drawable.mujer);
     }
 }
